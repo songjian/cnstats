150de3ba5fea5544bbe560f03b94e0a8c911ecaf
sj <songjian@codeorder.cn>
2022-03-10 23:29:08 +0800

    提交

--- a.py
+++ a.py
@@ -20,10 +20,19 @@ def random_timestamp():
 
 def stats_money_supply():
     url='https://data.stats.gov.cn/easyquery.htm?m=QueryData&dbcode=hgyd&rowcode=zb&colcode=sj&wds=[]&dfwds=[{"wdcode":"zb","valuecode":"A0D01"}]&k1='+random_timestamp()+'&h=1'
+    requests.packages.urllib3.disable_warnings()
     r = requests.get(url,headers=header, verify=False)
     ret=r.json()
     if ret['returncode'] == 200 :
-        return ret['returndata']
+        return ret['returndata']['datanodes'][1]
     return {}
 
+# m2dict={}
+# for node in stats_money_supply().datanodes:
+#     print(node)
+    # if node.data.hasdata == True and node.wds[0].valuecode == 'A0D0101':
+    #     m2dict[node['wds'][1]['valuecode']]=node['data']['data']
+
+
+# print(m2dict)
 print(stats_money_supply())
