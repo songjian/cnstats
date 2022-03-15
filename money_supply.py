ccbfc92fd1a9f26a134e972ea26ce93a3b87a057
sj <songjian@codeorder.cn>
2022-03-15 11:20:38 +0800

    优化获取货币供应量脚本

--- money_supply.py
+++ money_supply.py
@@ -6,6 +6,7 @@ import datetime
 import requests
 import json
 import time
+import sys
 
 header={
         'Accept-Encoding':'gzip, deflate, br',
@@ -13,14 +14,15 @@ header={
         'Referer':'https://data.stats.gov.cn/easyquery.htm?cn=A01',
         'Host':'data.stats.gov.cn',
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.36',
-        'X-Requested-With':'XMLHttpRequest'
+        'X-Requested-With':'XMLHttpRequest',
+        'Cookie':'_trs_uv=l0krufmy_6_30qm; JSESSIONID=JkGLaObMfWG3_P3_bNKa59cUydvE_nJDUpJOsskem4S-E-wgJeA7!-2135294552; u=1'
        }
 
 def random_timestamp():
     return str(int(round(time.time() * 1000)))
 
-def stats_money_supply():
-    url='https://data.stats.gov.cn/easyquery.htm?m=QueryData&dbcode=hgyd&rowcode=zb&colcode=sj&wds=[]&dfwds=[{"wdcode":"zb","valuecode":"A0D01"}]&k1='+random_timestamp()+'&h=1'
+def stats_money_supply(datestr):
+    url='https://data.stats.gov.cn/easyquery.htm?m=QueryData&dbcode=hgyd&rowcode=zb&colcode=sj&wds=[]&dfwds=[{"wdcode":"zb","valuecode":"A0D01"},{"wdcode":"sj","valuecode":"'+datestr+'"}]&k1='+random_timestamp()
     requests.packages.urllib3.disable_warnings()
     r = requests.get(url,headers=header, verify=False)
     ret=r.json()
@@ -29,4 +31,4 @@ def stats_money_supply():
             if n['data']['hasdata'] == True:
                 print(n['wds'][0]['valuecode'],n['wds'][1]['valuecode'],n['data']['data'])
 
-stats_money_supply() 
+stats_money_supply(sys.argv[1]) 
