750c84a5cedbab4a2587bd53bc8693b45a45c6c0
sj <songjian@codeorder.cn>
2022-03-16 13:20:39 +0800

    打包0.0.3版

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cn_stats",                                     # 包的分发名称，使用字母、数字、_、-
-    version="0.0.2",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
+    version="0.0.3",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
     author="sj",                                       # 作者名字
     author_email="songjian@codeorder.cn",                      # 作者邮箱
     description="获取中国国家统计局网站数据",                            # 包的简介描述
--- zbcode.py
+++ zbcode.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import sys
 from cnstats.zbcode import get_tree
 
-get_tree('zb')
+get_tree(sys.argv[1])
