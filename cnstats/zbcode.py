e7fc85b4de25777785d3dd215f245237eb0cd416
sj <songjian@codeorder.cn>
2022-03-16 11:03:17 +0800

    增加指标代码

---
+++ cnstats/zbcode.py
@@ -0,0 +1,23 @@
+import requests
+
+header={
+        'Accept-Encoding':'gzip, deflate, br',
+        'Connection':'keep-alive',
+        'Referer':'https://data.stats.gov.cn/easyquery.htm?cn=A01',
+        'Host':'data.stats.gov.cn',
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.36',
+        'X-Requested-With':'XMLHttpRequest',
+        'Cookie':'_trs_uv=l0krufmy_6_30qm; JSESSIONID=JkGLaObMfWG3_P3_bNKa59cUydvE_nJDUpJOsskem4S-E-wgJeA7!-2135294552; u=1'
+       }
+
+def easyquery(id='zb'):
+    url='https://data.stats.gov.cn/easyquery.htm'
+    obj={'id': id, 'dbcode': 'hgyd', 'wdcode': 'zb', 'm': 'getTree'}
+    requests.packages.urllib3.disable_warnings()
+    r=requests.post(url, data=obj, headers=header, verify=False)
+    return r.json()
+
+def get_tree(id='zb'):
+    for n in easyquery(id):
+        print(n['id'], n['name'])
+        if n['isParent']: get_tree(n['id'])
---
+++ zbcode.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from cnstats.zbcode import get_tree
+
+get_tree('zb')
