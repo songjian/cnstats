3288482cf83298ecc8b75558d507f0e1bf11e858
sj <songjian@codeorder.cn>
2022-03-30 17:51:35 +0800

    优化模块命令行

--- cnstats/__main__.py
+++ cnstats/__main__.py
@@ -1,5 +1,9 @@
-import sys
 from .stats import stats
+import argparse
 
 if __name__ == '__main__':
-    stats(sys.argv[1], sys.argv[2])
+    parser=argparse.ArgumentParser()
+    parser.add_argument('code', help='zbcode')
+    parser.add_argument('date', help='查询日期')
+    args=parser.parse_args()
+    stats(args.code, args.date)
