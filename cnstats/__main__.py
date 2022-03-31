b316b14e5c20064966311ef01e56832094d144e5
sj <songjian@codeorder.cn>
2022-03-31 11:03:51 +0800

    优化命令行参数

--- cnstats/__main__.py
+++ cnstats/__main__.py
@@ -1,9 +1,16 @@
+from ast import arg
 from .stats import stats
+from .zbcode import get_tree
 import argparse
 
 if __name__ == '__main__':
-    parser=argparse.ArgumentParser()
-    parser.add_argument('code', help='zbcode')
-    parser.add_argument('date', help='查询日期')
+    parser=argparse.ArgumentParser(prog='cn-stats',description='获取中国国家统计局网站数据Python包')
+    parser.add_argument('--tree', action='store_const', const='zb', help='列出指标代码')
+    parser.add_argument('--dbcode', nargs='?', const='hgyd', default='hgyd', help='数据库代码')
+    parser.add_argument('zbcode', help='指标代码', nargs='?')
+    parser.add_argument('date', help='查询日期', nargs='?')
     args=parser.parse_args()
-    stats(args.code, args.date)
+    if args.tree == 'zb':
+        get_tree(args.tree, args.dbcode)
+    else:
+        stats(args.zbcode, args.date)
