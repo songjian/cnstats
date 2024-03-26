a0e368bcaa3aeec15c2ed24d7a36d5d2be2de75b
sj <songjian@codeorder.cn>
2024-03-26 10:22:09 +0800

    发布0.0.8版本

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cn_stats",                                     # 包的分发名称，使用字母、数字、_、-
-    version="0.0.7",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
+    version="0.0.8",                                        # 版本号, 版本号规范：https://www.python.org/dev/peps/pep-0440/
     author="sj",                                       # 作者名字
     author_email="724385768@qq.com",                      # 作者邮箱
     description="获取中国国家统计局网站数据。",                            # 包的简介描述
